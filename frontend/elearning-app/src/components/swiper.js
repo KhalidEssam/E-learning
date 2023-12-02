220b9f50a2ea886022a816c56bd2eae6a998ed3f
Khaled <khaledesam384@gmail.com>
2023-12-02 20:02:06 +0200

    swipper added

---
+++ frontend/elearning-app/src/components/swiper.js
@@ -0,0 +1,24 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+const Slider = ({ slides }) => {
+    return (
+        <Swiper
+            spaceBetween={10}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {slides.map((slide) => (
+                <SwiperSlide key={slide.image}>
+                    <img src={slide.image} alt={slide.title} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+};
+
+export default Slider;
--- frontend/elearning-app/src/pages/Home.js
+++ frontend/elearning-app/src/pages/Home.js
@@ -2,12 +2,16 @@
 import React from 'react';
 import SignupForm from '../components/Signup';
 import SigninForm from '../components/Signin';
+import Slider from '../components/swiper';
+
+import slides from '../slides.json';
 
 
 function Home() {
     return (
         <div>
             <h1>Hello</h1>
+            <Slider slides={slides} />
             <SigninForm />
             <SignupForm />
         </div>
